d19bfc37d5034b131118c4fdf5de41620c623dc1
ColeyG <cjgeerts@gmail.com>
2019-04-23 17:24:35 -0400

    redesign

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,36 @@
-// anime({
-//     targets: '.secSplit',
-//     keyframes: [
-//         { rotate: 45 },
-//         { rotate: 90 },
-//         { rotate: 135 },
-//         { rotate: 180 },
-//         { rotate: 225 },
-//         { rotate: 270 },
-//         { rotate: 315 },
-//         { rotate: 360 },
-//     ],
-//     loop: true,
-//     duration: 8000,
-// })
+const bezier = '.64,1.53,1,1.07';
 
 anime({
-    targets: '.splitBox',
+    targets: '.leftBall',
     keyframes: [
-        { translateY: 40 },
-        { translateY: 40 },
-        { translateX: 40 },
-        { translateX: 40 },
-        { translateY: 0 },
-        { translateY: 0 },
-        { translateX: 0 },
-        { translateX: 0 },
+        { rotate: 45 },
+        { rotate: 0 },
+        { rotate: 0 },
     ],
     loop: true,
-    duration: 8000,
-    easing: 'cubicBezier(.5, .05, .1, .3)',
+    easing: 'cubicBezier(' + bezier + ')',
+})
+
+anime({
+    targets: '.rightBall',
+    keyframes: [
+        { rotate: 0 },
+        { rotate: 0 },
+        { rotate: -45 },
+        { rotate: 0 },
+    ],
+    loop: true,
+    easing: 'cubicBezier(' + bezier + ')',
+})
+
+anime({
+    targets: '.mainBall',
+    keyframes: [
+        { rotate: 5 },
+        { rotate: 0 },
+        { rotate: -5 },
+        { rotate: 0 },
+    ],
+    loop: true,
+    easing: 'cubicBezier(' + bezier + ')',
 })
