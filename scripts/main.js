b18cb6110c80c3dab34b3051669a99cc384b8ef4
ColeyG <cjgeerts@gmail.com>
2019-08-29 13:01:41 -0400

    update index

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,31 @@
-const bezier = '.64,1.53,1,1.07';
+const bezier = ".64,1.53,1,1.07";
 
 anime({
-    targets: '.leftBall',
-    keyframes: [
-        { rotate: 45 },
-        { rotate: 0 },
-        { rotate: 0 },
-    ],
-    loop: true,
-    easing: 'cubicBezier(' + bezier + ')',
-})
+  targets: ".leftBall",
+  keyframes: [{ rotate: 45 }, { rotate: 0 }, { rotate: 0 }],
+  loop: true,
+  easing: "cubicBezier(" + bezier + ")",
+});
 
 anime({
-    targets: '.rightBall',
-    keyframes: [
-        { rotate: 0 },
-        { rotate: 0 },
-        { rotate: -45 },
-        { rotate: 0 },
-    ],
-    loop: true,
-    easing: 'cubicBezier(' + bezier + ')',
-})
+  targets: ".rightBall",
+  keyframes: [{ rotate: 0 }, { rotate: 0 }, { rotate: -45 }, { rotate: 0 }],
+  loop: true,
+  easing: "cubicBezier(" + bezier + ")",
+});
 
 anime({
-    targets: '.mainBall',
-    keyframes: [
-        { rotate: 5 },
-        { rotate: 0 },
-        { rotate: -5 },
-        { rotate: 0 },
-    ],
-    loop: true,
-    easing: 'cubicBezier(' + bezier + ')',
-})
+  targets: ".mainBall",
+  keyframes: [{ rotate: 5 }, { rotate: 0 }, { rotate: -5 }, { rotate: 0 }],
+  loop: true,
+  easing: "cubicBezier(" + bezier + ")",
+});
+
+// let images = document.querySelectorAll(".cardCold img");
+
+// images.forEach(element => {
+//   element.parentNode.style.backgroundImage = "url(" + element.src + ")";
+//   element.style.display = "none";
+// });
+
+// console.log(images);
