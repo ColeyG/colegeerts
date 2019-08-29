1a9dedcd8afd5338b254d8908c6760a3cf459a9a
ColeyG <cjgeerts@gmail.com>
2019-08-29 13:57:45 -0400

    Blur image script fixes

--- images/image-blurring.sh
+++ images/image-blurring.sh
@@ -1,6 +1,17 @@
-for D in `find . -type d`
+for D in `find ./*.png -type f`
 do
-    convert D -blur 0x8 -fill black -colorize 30% blurred/D
+    convert ${D} -blur 0x8 -fill black -colorize 30% blurred/${D}
+    echo ${D}
 done
 
+for D in `find ./*.jpg -type f`
+do
+    convert ${D} -blur 0x8 -fill black -colorize 30% blurred/${D}
+    echo ${D}
+done
 
+for D in `find ./*.jpeg -type f`
+do
+    convert ${D} -blur 0x8 -fill black -colorize 30% blurred/${D}
+    echo ${D}
+done
