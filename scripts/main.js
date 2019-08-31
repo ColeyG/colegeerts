b65d29a0958aa1ff68eb3c43278a6296947362d2
ColeyG <cjgeerts@gmail.com>
2019-08-31 19:02:24 -0400

    Add resume | Page switcher

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,43 @@ anime({
   easing: "cubicBezier(" + bezier + ")",
 });
 
-let images = document.querySelectorAll(".cardCold img");
+let images = document.querySelectorAll(".cardCold img"),
+  pageButtons = document.querySelectorAll(".js-page-switch"),
+  contactClose = document.querySelectorAll(".js-contact-close"),
+  wrapper = document.querySelector(".wrapper");
+
+const addClass = (element, cssClass) => {
+  element.className = element.className + " " + cssClass;
+};
+
+const removeClass = (element, cssClass) => {
+  //TODO: Not perfect, need to remove space around
+  element.className = element.className.split(cssClass).join("");
+};
+
+const pageSwitch = page => {
+  wrapper.style.opacity = 0;
+  setTimeout(() => {
+    let sections = document.querySelectorAll("[data-page]");
+
+    sections.forEach(element => {
+      addClass(document.querySelector("." + element.dataset.page), "sec-hidden");
+    });
+
+    removeClass(document.querySelector("." + page), "sec-hidden");
+    wrapper.style.opacity = 1;
+  }, 1000);
+};
+
+pageButtons.forEach(element => {
+  element.addEventListener(
+    "click",
+    () => {
+      pageSwitch(event.target.dataset.page);
+    },
+    false
+  );
+});
 
 images.forEach(element => {
   element.parentNode.style.backgroundImage = "url(" + element.src + ")";
