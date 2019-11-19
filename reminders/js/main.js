47d6cffe7b281acb8482953b17bd534f1f22926b
ColeyG <cjgeerts@gmail.com>
2019-11-19 15:00:25 -0500

    Update JS

--- reminders/js/main.js
+++ reminders/js/main.js
@@ -1,117 +1,120 @@
-const color = Math.random() * 360;
-document.querySelector('.wrapper').style.backgroundColor = `hsl(${color},70%,70%)`;
-const thing = document.querySelector('#actionItem');
-const time = document.querySelector('#time');
-const submit = document.querySelector('#submit');
-const deleteButtons = document.querySelectorAll('.delete');
-const successButtons = document.querySelectorAll('.complete');
-const resetButtons = document.querySelectorAll('.reset');
-const removeButtons = document.querySelectorAll('.remove');
+(() => {
+  const color = Math.random() * 360;
+  document.querySelector('.wrapper').style.backgroundColor = `hsl(${color},70%,70%)`;
+  const thing = document.querySelector('#actionItem');
+  const time = document.querySelector('#time');
+  const submit = document.querySelector('#submit');
+  const deleteButtons = document.querySelectorAll('.delete');
+  const successButtons = document.querySelectorAll('.complete');
+  const resetButtons = document.querySelectorAll('.reset');
+  const removeButtons = document.querySelectorAll('.remove');
 
-// Response for
-function returned(data, location) {
-  console.log(data);
-  if (data.includes('success')) {
-    location.reload(true);
+  // Response for
+  function returned(data) {
+    console.log(data);
+    if (data.includes('success')) {
+      // eslint-disable-next-line no-restricted-globals
+      location.reload(true);
+    }
   }
-}
 
-// Actions
-function deleteItem(e) {
-  e.preventDefault();
-  coldAjax('GET', `ajaxScripts/delete.php?id=${this.id}&code=2`, returned);
-}
+  // Actions
+  function deleteItem(e) {
+    e.preventDefault();
+    coldAjax('GET', `ajaxScripts/delete.php?id=${this.id}&code=2`, returned);
+  }
 
-function successItem(e) {
-  e.preventDefault();
-  coldAjax('GET', `ajaxScripts/delete.php?id=${this.id}&code=1`, returned);
-}
+  function successItem(e) {
+    e.preventDefault();
+    coldAjax('GET', `ajaxScripts/delete.php?id=${this.id}&code=1`, returned);
+  }
 
-function resetItem(e) {
-  e.preventDefault();
-  coldAjax('GET', `ajaxScripts/delete.php?id=${this.id}&code=0`, returned);
-}
+  function resetItem(e) {
+    e.preventDefault();
+    coldAjax('GET', `ajaxScripts/delete.php?id=${this.id}&code=0`, returned);
+  }
 
-function removeItem(e) {
-  e.preventDefault();
-  coldAjax('GET', `ajaxScripts/delete.php?id=${this.id}&code=3`, returned);
-}
+  function removeItem(e) {
+    e.preventDefault();
+    coldAjax('GET', `ajaxScripts/delete.php?id=${this.id}&code=3`, returned);
+  }
 
-function submitAct() {
-  fetch(`ajaxScripts/sendVal.php?thing=${thing.value}&time=${time.value}`, { method: 'GET' })
-    .then((resp) => resp.text())
-    .then((data) => {
-      returned(data);
+  function submitAct() {
+    fetch(`ajaxScripts/sendVal.php?thing=${thing.value}&time=${time.value}`, { method: 'GET' })
+      .then((resp) => resp.text())
+      .then((data) => {
+        returned(data);
+      });
+  }
+
+  function sortCallback(data) {
+    console.log(data);
+  }
+
+  function saveAll() {
+    const allItems = document.querySelectorAll('.items ul li');
+    let sortVal = 0;
+    let sort = '';
+    let sortValues = '';
+    allItems.forEach((element) => {
+      sortVal++;
+      sort += `${element.id.replace('li', '')},`;
+      sortValues += `${sortVal},`;
     });
-}
 
-function sortCallback(data) {
-  console.log(data);
-}
+    sort = sort.slice(0, -1);
+    sortValues = sortValues.slice(0, -1);
 
-function saveAll() {
-  const allItems = document.querySelectorAll('.items ul li');
-  let sortVal = 0;
-  let sort = '';
-  let sortValues = '';
-  allItems.forEach((element) => {
-    sortVal++;
-    sort += `${element.id.replace('li', '')},`;
-    sortValues += `${sortVal},`;
+    console.log(sort, sortValues);
+
+    fetch(`ajaxScripts/sort.php?sort=${sort}&sortVal=${sortValues}`, { method: 'GET' })
+      .then((resp) => resp.text())
+      .then((data) => {
+        sortCallback(data);
+      });
+  }
+
+  // Event Bindings
+  submit.addEventListener('click', submitAct, false);
+
+  deleteButtons.forEach((element) => {
+    element.addEventListener('click', deleteItem, false);
   });
 
-  sort = sort.slice(0, -1);
-  sortValues = sortValues.slice(0, -1);
+  successButtons.forEach((element) => {
+    element.addEventListener('click', successItem, false);
+  });
 
-  console.log(sort, sortValues);
+  resetButtons.forEach((element) => {
+    element.addEventListener('click', resetItem, false);
+  });
 
-  fetch(`ajaxScripts/sort.php?sort=${sort}&sortVal=${sortValues}`, { method: 'GET' })
-    .then((resp) => resp.text())
-    .then((data) => {
-      sortCallback(data);
-    });
-}
+  removeButtons.forEach((element) => {
+    element.addEventListener('click', removeItem, false);
+  });
 
-// Event Bindings
-submit.addEventListener('click', submitAct, false);
-
-deleteButtons.forEach((element) => {
-  element.addEventListener('click', deleteItem, false);
-});
-
-successButtons.forEach((element) => {
-  element.addEventListener('click', successItem, false);
-});
-
-resetButtons.forEach((element) => {
-  element.addEventListener('click', resetItem, false);
-});
-
-removeButtons.forEach((element) => {
-  element.addEventListener('click', removeItem, false);
-});
-
-// Sortables Initializations
-new Sortable(document.querySelector('#todo-sortable'), {
-  animation: 150,
-  ghostClass: 'blue-background-class',
-  store: {
-    set: () => {
-      saveAll();
+  // Sortables Initializations
+  new Sortable(document.querySelector('#todo-sortable'), {
+    animation: 150,
+    ghostClass: 'blue-background-class',
+    store: {
+      set: () => {
+        saveAll();
+      },
     },
-  },
-  dataIdAttr: 'data-id',
-  handle: '.handle',
-});
+    dataIdAttr: 'data-id',
+    handle: '.handle',
+  });
 
-new Sortable(document.querySelector('#done-sortable'), {
-  animation: 150,
-  ghostClass: 'blue-background-class',
-  store: {
-    set: () => {
-      saveAll();
+  new Sortable(document.querySelector('#done-sortable'), {
+    animation: 150,
+    ghostClass: 'blue-background-class',
+    store: {
+      set: () => {
+        saveAll();
+      },
     },
-  },
-  dataIdAttr: 'data-id',
-  handle: '.handle',
-});
+    dataIdAttr: 'data-id',
+    handle: '.handle',
+  });
+})();
--- scripts/main.js
+++ scripts/main.js
@@ -1,186 +1,188 @@
-// Animation library configuration
-const bezier = '.64,1.53,1,1.07';
+(() => {
+  // Animation library configuration
+  const bezier = '.64,1.53,1,1.07';
 
-anime({
-  targets: '.leftBall',
-  keyframes: [{ rotate: 45 }, { rotate: 0 }, { rotate: 0 }],
-  loop: true,
-  easing: `cubicBezier(${bezier})`,
-});
+  anime({
+    targets: '.leftBall',
+    keyframes: [{ rotate: 45 }, { rotate: 0 }, { rotate: 0 }],
+    loop: true,
+    easing: `cubicBezier(${bezier})`,
+  });
 
-anime({
-  targets: '.rightBall',
-  keyframes: [{ rotate: 0 }, { rotate: 0 }, { rotate: -45 }, { rotate: 0 }],
-  loop: true,
-  easing: `cubicBezier(${bezier})`,
-});
+  anime({
+    targets: '.rightBall',
+    keyframes: [{ rotate: 0 }, { rotate: 0 }, { rotate: -45 }, { rotate: 0 }],
+    loop: true,
+    easing: `cubicBezier(${bezier})`,
+  });
 
-anime({
-  targets: '.mainBall',
-  keyframes: [{ rotate: 5 }, { rotate: 0 }, { rotate: -5 }, { rotate: 0 }],
-  loop: true,
-  easing: `cubicBezier(${bezier})`,
-});
+  anime({
+    targets: '.mainBall',
+    keyframes: [{ rotate: 5 }, { rotate: 0 }, { rotate: -5 }, { rotate: 0 }],
+    loop: true,
+    easing: `cubicBezier(${bezier})`,
+  });
 
-// General query selectors
-const images = document.querySelectorAll('.cardCold img');
-const pageButtons = document.querySelectorAll('.js-page-switch');
-const wrapper = document.querySelector('.wrapper');
-const contactButton = document.querySelector('.contact-form-confirm');
+  // General query selectors
+  const images = document.querySelectorAll('.cardCold img');
+  const pageButtons = document.querySelectorAll('.js-page-switch');
+  const wrapper = document.querySelector('.wrapper');
+  const contactButton = document.querySelector('.contact-form-confirm');
 
-// Configuration object
-const config = {
-  pageTimer: 500,
-  defaultAlertColor: '#3c3c3c',
-  cautionAlertColor: '#DEEFB7',
-  errorAlertColor: '#B3001B',
-};
+  // Configuration object
+  const config = {
+    pageTimer: 500,
+    defaultAlertColor: '#3c3c3c',
+    cautionAlertColor: '#DEEFB7',
+    errorAlertColor: '#B3001B',
+  };
 
-// Add class utility function
-const addClass = (element, cssClass) => {
-  element.className = `${element.className} ${cssClass}`;
-};
+  // Add class utility function
+  const addClass = (element, cssClass) => {
+    element.className = `${element.className} ${cssClass}`;
+  };
 
-// Remove class utility function
-const removeClass = (element, cssClass) => {
-  element.className = element.className.split(cssClass).join('');
-  element.className = element.className.replace('  ', ' ').trim();
-};
+  // Remove class utility function
+  const removeClass = (element, cssClass) => {
+    element.className = element.className.split(cssClass).join('');
+    element.className = element.className.replace('  ', ' ').trim();
+  };
 
-// Page switcher function
-const pageSwitch = (page) => {
-  wrapper.style.opacity = 0;
-  setTimeout(() => {
-    const sections = document.querySelectorAll('[data-page]');
-
-    sections.forEach((element) => {
-      addClass(document.querySelector(`.${element.dataset.page}`), 'sec-hidden');
-    });
-
-    removeClass(document.querySelector(`.${page}`), 'sec-hidden');
-    wrapper.style.opacity = 1;
-  }, config.pageTimer);
-};
-
-// Popup alert function
-const pageAlert = (alertText, offset = config.pageTimer, color = config.defaultAlertColor) => {
-  console.log(alertText);
-  setTimeout(() => {
-    const alertPopup = document.querySelector('.alert-popup');
-    const alertPopupText = document.querySelector('.alert-popup-text');
-    alertPopup.style.backgroundColor = color;
-    alertPopupText.innerHTML = alertText;
-    alertPopup.style.bottom = `${100}px`;
+  // Page switcher function
+  const pageSwitch = (page) => {
+    wrapper.style.opacity = 0;
     setTimeout(() => {
-      alertPopup.style.bottom = `${-150}px`;
-    }, config.pageTimer * 4);
-  }, offset * 2);
-};
+      const sections = document.querySelectorAll('[data-page]');
 
-// Callback function for contact ajax call
-const submitContactResponse = (data) => {
-  if (data.includes('success')) {
-    console.log('success mail submission');
-    pageAlert('Sent Message');
-    pageSwitch('main-content');
-  } else if (data.includes('failed')) {
-    console.log('failed mail submission');
-    pageAlert('Failed to Send Message', 0, config.errorAlertColor);
-  }
-  console.log(data);
-};
+      sections.forEach((element) => {
+        addClass(document.querySelector(`.${element.dataset.page}`), 'sec-hidden');
+      });
 
-// Contact submission
-const submitContact = () => {
-  const name = document.querySelector('#name').value;
-  const email = document.querySelector('#email').value;
-  const message = document.querySelector('#message').value;
-  let error = '';
+      removeClass(document.querySelector(`.${page}`), 'sec-hidden');
+      wrapper.style.opacity = 1;
+    }, config.pageTimer);
+  };
 
-  if (name === '' || email === '' || message === '') {
-    if (error === '') {
-      error = 'Missing one or more field!';
+  // Popup alert function
+  const pageAlert = (alertText, offset = config.pageTimer, color = config.defaultAlertColor) => {
+    console.log(alertText);
+    setTimeout(() => {
+      const alertPopup = document.querySelector('.alert-popup');
+      const alertPopupText = document.querySelector('.alert-popup-text');
+      alertPopup.style.backgroundColor = color;
+      alertPopupText.innerHTML = alertText;
+      alertPopup.style.bottom = `${100}px`;
+      setTimeout(() => {
+        alertPopup.style.bottom = `${-150}px`;
+      }, config.pageTimer * 4);
+    }, offset * 2);
+  };
+
+  // Callback function for contact ajax call
+  const submitContactResponse = (data) => {
+    if (data.includes('success')) {
+      console.log('success mail submission');
+      pageAlert('Sent Message');
+      pageSwitch('main-content');
+    } else if (data.includes('failed')) {
+      console.log('failed mail submission');
+      pageAlert('Failed to Send Message', 0, config.errorAlertColor);
     }
-  }
+    console.log(data);
+  };
 
-  if (!email.includes('@') || !email.includes('.')) {
-    if (error === '') {
-      error = 'Not a valid email address!';
+  // Contact submission
+  const submitContact = () => {
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+    let error = '';
+
+    if (name === '' || email === '' || message === '') {
+      if (error === '') {
+        error = 'Missing one or more field!';
+      }
     }
-  }
 
-  if (error === '') {
-    coldAjax('GET', `ajaxScripts/mail.php?name=${name}&email=${email}&message=${message}`, submitContactResponse);
-  } else {
-    pageAlert(error, 0, config.errorAlertColor);
-  }
-};
+    if (!email.includes('@') || !email.includes('.')) {
+      if (error === '') {
+        error = 'Not a valid email address!';
+      }
+    }
 
-// Getting average image color
-const getAverageImageColor = (imgEl) => {
-  const sampleRate = 5;
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext && canvas.getContext('2d');
-  let data;
-  let i = -4;
-  const rgb = { r: 0, g: 0, b: 0 };
-  let count = 0;
+    if (error === '') {
+      coldAjax('GET', `ajaxScripts/mail.php?name=${name}&email=${email}&message=${message}`, submitContactResponse);
+    } else {
+      pageAlert(error, 0, config.errorAlertColor);
+    }
+  };
 
-  if (!context) {
-    return 'unsupported context';
-  }
+  // Getting average image color
+  const getAverageImageColor = (imgEl) => {
+    const sampleRate = 5;
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext && canvas.getContext('2d');
+    let data;
+    let i = -4;
+    const rgb = { r: 0, g: 0, b: 0 };
+    let count = 0;
 
-  const height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-  const width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+    if (!context) {
+      return 'unsupported context';
+    }
 
-  context.drawImage(imgEl, 0, 0);
+    const height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    const width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
 
-  try {
-    data = context.getImageData(0, 0, width, height);
-  } catch (e) {
-    /* security error, image on different domain */
-    console.log('unsupported context - unable to get average image color');
-    return 'unsupported context';
-  }
+    context.drawImage(imgEl, 0, 0);
 
-  const length = data.data;
+    try {
+      data = context.getImageData(0, 0, width, height);
+    } catch (e) {
+      /* security error, image on different domain */
+      console.log('unsupported context - unable to get average image color');
+      return 'unsupported context';
+    }
 
-  /* eslint no-cond-assign: "off" */
-  while ((i += sampleRate * 4) < length) {
-    ++count;
-    rgb.r += data.data[i];
-    rgb.g += data.data[i + 1];
-    rgb.b += data.data[i + 2];
-  }
+    const length = data.data;
 
-  rgb.r = ~~(rgb.r / count);
-  rgb.g = ~~(rgb.g / count);
-  rgb.b = ~~(rgb.b / count);
+    /* eslint no-cond-assign: "off" */
+    while ((i += sampleRate * 4) < length) {
+      ++count;
+      rgb.r += data.data[i];
+      rgb.g += data.data[i + 1];
+      rgb.b += data.data[i + 2];
+    }
 
-  return rgb;
-};
+    rgb.r = ~~(rgb.r / count);
+    rgb.g = ~~(rgb.g / count);
+    rgb.b = ~~(rgb.b / count);
 
-// Event Listeners
-// Page changing buttons
-pageButtons.forEach((element) => {
-  element.addEventListener(
-    'click',
-    (event) => {
-      pageSwitch(event.target.dataset.page);
-    },
-    false,
-  );
-});
+    return rgb;
+  };
 
-// Card images
-images.forEach((element) => {
-  const averageColor = getAverageImageColor(element);
-  element.parentNode.style.backgroundImage = `url(${element.src})`;
-  element.parentNode.style.boxShadow = `5px 5px 10px rgba(0, 0, 0, .75), 5px 5px 10px rgba(${averageColor.r},${averageColor.g},${averageColor.b},.75)`;
-  element.remove();
-});
+  // Event Listeners
+  // Page changing buttons
+  pageButtons.forEach((element) => {
+    element.addEventListener(
+      'click',
+      (event) => {
+        pageSwitch(event.target.dataset.page);
+      },
+      false,
+    );
+  });
 
-// Contact submission button
-contactButton.addEventListener('click', submitContact, false);
+  // Card images
+  images.forEach((element) => {
+    const averageColor = getAverageImageColor(element);
+    element.parentNode.style.backgroundImage = `url(${element.src})`;
+    element.parentNode.style.boxShadow = `5px 5px 10px rgba(0, 0, 0, .75), 5px 5px 10px rgba(${averageColor.r},${averageColor.g},${averageColor.b},.75)`;
+    element.remove();
+  });
 
-pageSwitch('main-content');
+  // Contact submission button
+  contactButton.addEventListener('click', submitContact, false);
+
+  pageSwitch('main-content');
+})();
